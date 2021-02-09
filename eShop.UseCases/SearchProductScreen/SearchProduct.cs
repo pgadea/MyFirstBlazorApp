ce09fa52200b4e382eedc8fdf70e2eb483930844
Pierre <pgadea@users.noreply.github.com>
2021-02-09 17:02:53 -0600

    Add project files.

---
+++ eShop.UseCases/SearchProductScreen/SearchProduct.cs
@@ -0,0 +1,22 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+    public class SearchProduct
+    {
+        private readonly IProductRepository productRepository;
+
+        public SearchProduct(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public IEnumerable<Product> Execute(string filter)
+        {
+            return productRepository.GetProducts(filter);
+        }
+    }
+}
